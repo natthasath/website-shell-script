7046517a3badec11023663e98d49935791e6d142
natthasath <natthasath.sak@gmail.com>
2023-01-30 10:45:44 +0700

    Update scripts

--- scripts/check-version-wordpress.sh
+++ scripts/check-version-wordpress.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-url=$1
+# Prompt for the URL
+read -p "Enter the URL of the WordPress site: " url
 
 # Get the version of WordPress from the meta tag
-version=$(curl -s $url | grep "generator" | awk -F 'content=' '{print $2}' | awk -F '"' '{print $2}')
+version=$(curl -s "$url" | grep "generator" | awk -F 'content=' '{print $2}' | awk -F '"' '{print $2}')
 
 # Print the version number
 echo "WordPress version: $version"
--- scripts/increase-max-size.sh
+++ scripts/increase-max-size.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-upload=$1
-post=$2
+# Prompt for the upload max filesize
+read -p "Enter the new maximum size for upload_max_filesize (in MB): " upload_max_filesize
 
+# Prompt for the post max size
+read -p "Enter the new maximum size for post_max_size (in MB): " post_max_size
+
+# Prompt for the path to php.ini
+read -p "Enter the path to php.ini: " file_path
+
+# Check if the file exists
+if [ ! -f $file_path ]; then
+  echo "Error: $file_path does not exist."
+  exit 1
+fi
+
+# Create a backup of the file with a timestamp
+backup_file="$file_path-$(date +%Y%m%d%H%M%S).bak"
+cp $file_path $backup_file
+
+# Set the new maximum size in php.ini
 sed -i \
--e "s/\(upload_max_filesize = \).*/\1$1/" \
--e "s/\(post_max_size = \).*/\1$2/" \
-php.ini
+-e "s/^\(\s*upload_max_filesize\s*=\s*\).*$/\1${upload_max_filesize}M/" \
+-e "s/^\(\s*post_max_size\s*=\s*\).*$/\1${post_max_size}M/" \
+$php_ini_path
+
+# Restart Apache to apply the changes
+service apache2 restart
