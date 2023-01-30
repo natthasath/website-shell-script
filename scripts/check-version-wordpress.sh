3072ee54538a123e1b849c818f94bfb1b954c020
natthasath <natthasath.sak@gmail.com>
2023-01-30 09:52:25 +0700

    Update scripts

---
+++ scripts/check-version-wordpress.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+url=$1
+
+# Get the version of WordPress from the meta tag
+version=$(curl -s $1 | grep "generator" | awk -F 'content=' '{print $2}' | awk -F '"' '{print $2}')
+
+# Print the version number
+echo "WordPress version: $version"
