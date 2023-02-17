859da6544e7f79e79bf41f6e54fc627b3c6a0e87
natthasath <natthasath.sak@gmail.com>
2023-02-17 09:34:53 +0700

    Add Force Download File PHP

---
+++ forced/download.php
@@ -0,0 +1,15 @@
+<?php
+
+$filename = 'download.ovpn';
+
+if (file_exists($filename)) {
+	header('Content-Description: File Transfer');
+	header('Content-Type: application/octet-stream');
+	header('Content-Disposition: attachment; filename="' . basename($filename) . '"');
+	header('Expires: 0');
+	header('Cache-Control: must-revalidate');
+	header('Pragma: public');
+	header('Content-Length: ' . filesize($filename));
+	readfile($filename);
+	exit;
+}
